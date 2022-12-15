c858abe80028f128378639c3e6b27d8cb867fd18
Daniël Mizrahi <dmizrahi@live.nl>
2022-12-15 13:01:01 +0100

    Updated read_dataset to drop columns with 1 unique value

--- read_data.py
+++ read_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
 
-def read_dataset(path: str, one_hot_encoding=False, drop_first=True) -> pd.DataFrame:
+def read_dataset(path: str, process=False, drop_first=True) -> pd.DataFrame:
     data = pd.read_csv(path, encoding='utf-8')
-    if one_hot_encoding:
-        data_one_hot_encoded = pd.get_dummies(data, drop_first=drop_first)
-        return data_one_hot_encoded
+    if process:
+        processed = pd.get_dummies(data, drop_first=drop_first)
+        irrelevant_columns = [column for column in processed.columns if processed[column].nunique() == 1]
+        processed.drop(irrelevant_columns, axis=1, inplace=True)
+        return processed
     else:
         return data
