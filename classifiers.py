3bbbc41c835bbe0c57fbb006c0605abde450226e
Daniël Mizrahi <dmizrahi@live.nl>
2022-12-15 13:00:24 +0100

    Added NaiveBayesClassifier
    Needs testing.

--- classifiers.py
+++ classifiers.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 
 
 class LogisticRegressionClassifier:
@@ -17,3 +18,17 @@ class LogisticRegressionClassifier:
             return y[0]
 
 
+class NaiveBayesClassifier:
+    def __init__(self) -> None:
+        self.model = GaussianNB()
+
+    def fit(self, X, y) -> None:
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        if len(X.shape) >= 2:
+            y = self.model.predict(X)
+            return y
+        else:
+            y = self.model.predict([X])
+            return y[0]
