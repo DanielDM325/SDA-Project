1711e94d46eb344686b04fd3875c707f6f578cee
Daniël Mizrahi <dmizrahi@live.nl>
2022-12-24 17:33:03 +0100

    Added F-score

--- performance.py
+++ performance.py
@@ -9,3 +9,7 @@ def precision(test_data: pd.DataFrame, prediction_data: pd.DataFrame, for_class:
 
 def recall(test_data: pd.DataFrame, prediction_data: pd.DataFrame, for_class: int = 1) -> float:
     return recall_score(test_data['fraud_bool'], prediction_data, pos_label=for_class, average='binary')
+
+
+def f_score(precision: float, recall: float) -> float:
+    return 2 / (1 / precision + 1 / recall)
