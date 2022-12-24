ffcfaaadf21772c464f24b8ff6fd7d8ae7a0e214
Daniël Mizrahi <dmizrahi@live.nl>
2022-12-24 17:09:14 +0100

    pearson_correlation_coefficient now returns p-values too

--- data_analysis.py
+++ data_analysis.py
@@ -52,7 +52,7 @@ def cross_correlation_coefficient(sample_1, sample_2=None):
 
 def pearson_correlation_coefficient(sample_1, sample_2=None):
     if type(sample_1) == np.ndarray and type(sample_2) == np.ndarray:
-        correlation = stats.pearsonr(sample_1, sample_2)[0]
+        correlation = stats.pearsonr(sample_1, sample_2)
         return correlation
     elif type(sample_1) == pd.DataFrame:
         return sample_1.corr('pearson')
